5169bd1abeacef0483f574d8e06fa3c8c38d4554
hafizip00 <hafizip00@gmail.com>
2022-07-16 01:05:42 +0500

    First Commit:)

---
+++ src/main/java/ipenter/annotation/App.java
@@ -0,0 +1,14 @@
+package ipenter.annotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Samsung s7 = new Samsung();
+        s7.Config();
+    }
+}
---
+++ src/main/java/ipenter/annotation/Samsung.java
@@ -0,0 +1,7 @@
+package ipenter.annotation;
+
+public class Samsung {
+	public  void Config() {
+		System.out.println("S7 Mobile");
+	}
+}
